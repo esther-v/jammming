be802fbef9ec4f62505e786f07d6d61cb0afacb3
esther-v <esvalas@gmail.com>
2022-04-09 17:33:45 +0200

    implement spotify access token

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -6,6 +6,9 @@ class SearchBar extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      term: ""
+    }
     this.search = this.search.bind(this)
     this.handleTermChange = this.handleTermChange.bind(this)
   }
---
+++ src/util/Spotify.js
@@ -0,0 +1,30 @@
+const clientId = process.env.REACT_APP_CLIENT_ID
+const redirectUri = process.env.REACT_APP_REDIRECT_URI
+
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken
+        }
+
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1]
+            const expiresIn = Number(expiresInMatch[1])
+            //this clears the parameters:
+            window.setTimeout(() => accessToken = '', expiresIn * 1000)
+            window.history.pushState('Access Token', null, '/')
+            return accessToken
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
+            window.location = accessUrl
+        }
+
+    }
+}
+
+export default Spotify
